d63a173f7d4e463150e504ecc6d56b5da99a2725
Alexander Abnosov <abnosov@gmail.com>
2021-04-05 17:43:53 +0300

    Экспорт данных пластики и салонов для последующей записи в БД

--- app/modules/utils.py
+++ app/modules/utils.py
@@ -20,3 +20,70 @@ class WriteServicesToDB:
 
             db.session.add(plastic_services)
             db.session.commit()
+
+
+class ManagePlasticDescriptions:
+    def __init__(self):
+        plastic_services = PlasticServices.query.all()
+
+        for p in plastic_services:
+            if p.description is None:
+                if "(" in p.service:
+                    print(p.service)
+                    description = p.service.rsplit("(", 1)[1].rsplit(")", 1)[0]
+                    p.description = description
+                    db.session.commit()
+
+
+class ManageBeautyDescriptions:
+    def __init__(self):
+        beauty_services = BeautyServices.query.all()
+
+        for b in beauty_services:
+            if b.description is None:
+                if "(" in b.service:
+                    print(b.description)
+                    print(b.service)
+                    description = b.service.rsplit("(", 1)[1].rsplit(")", 1)[0]
+                    b.description = description
+                    db.session.commit()
+
+
+class ExportPlasticToJSON:
+    def __init__(self):
+        plastic_services = PlasticServices.query.all()
+
+        plastic_dicts = []
+
+        for p in plastic_services:
+            plastic_dicts.append(
+                dict(
+                    service=p.service,
+                    id=p.id,
+                    description=p.description,
+                )
+            )
+
+        new_data = json.dumps(plastic_dicts, indent=2, ensure_ascii=False)
+        with open("./data/surgery/new_data/services.json", "w+") as json_file:
+            json_file.write(new_data)
+
+
+class ExportBeautyToJSON:
+    def __init__(self):
+        beauty_services = PlasticServices.query.all()
+
+        beauty_dicts = []
+
+        for b in beauty_services:
+            beauty_dicts.append(
+                dict(
+                    service=b.service,
+                    id=b.id,
+                    description=b.description,
+                )
+            )
+
+        new_data = json.dumps(beauty_dicts, indent=2, ensure_ascii=False)
+        with open("./data/beauty/new_data/services.json", "w+") as json_file:
+            json_file.write(new_data)
